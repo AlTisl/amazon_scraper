09b6d4570b9e0b9ba1a641031bef0266fe212f61
Olexandr Tislichenko <47416402+AlTisl@users.noreply.github.com>
2025-08-05 16:53:18 +0300

    Update interactions.py

--- database/interactions.py
+++ database/interactions.py
@@ -1,7 +1,7 @@
 from sqlite3 import connect, Row
 from typing import Any
 
-def db_create_table(db_name: str):
+def db_create_table(db_name: str) -> None:
     with connect('.\\' + db_name) as con:
         con.execute('''
             CREATE TABLE IF NOT EXISTS products (
@@ -17,7 +17,7 @@ def db_create_table(db_name: str):
         ''')
     con.close()
 
-def db_insert(db_name: str, data: list[dict[str, Any]]):
+def db_insert(db_name: str, data: list[dict[str, Any]]) -> None:
     
     with connect('.\\' + db_name) as con:
         con.executemany('''
@@ -42,8 +42,58 @@ def db_select_all(db_name: str) -> list[dict[str, Any]]:
     con.close()
     return row
 
-def db_truncate_table(db_name: str):
+def db_truncate_table(db_name: str) -> None:
     with connect('.\\' + db_name) as con:
         con.execute('DROP TABLE IF EXISTS products')
     con.close()
     db_create_table(db_name)
+
+# Розрахунок середньої ціни товарів з рейтингом більшим за 4
+def db_average_price(db_name: str) -> float:
+    with connect('.\\' + db_name) as con:
+        result = con.execute('''
+                    SELECT AVG(CAST(current_price AS REAL)/100) AS avg_price
+                    FROM products
+                    ''').fetchone()
+    con.close()
+    return result[0]
+
+# Визначення товару з найбільшим дисконтом
+def db_max_discount(db_name: str) -> dict[str, Any]:
+    with connect('.\\' + db_name) as con:
+        con.row_factory = Row
+        result = con.execute('''
+                    SELECT title, url, rating, reviews,
+                           CAST(current_price AS REAL)/100 AS current_price,
+                           CAST(base_price AS REAL)/100 AS base_price,
+                           (CASE delivery
+                                WHEN 1 THEN 'Yes'
+                                ELSE 'No'
+                           END) AS delivery
+                    FROM products
+                    WHERE current_price IS NOT NULL AND base_price IS NOT NULL
+                    ORDER BY (base_price - current_price) DESC
+                    LIMIT 1
+                    ''').fetchone()
+    con.close()
+    return result
+
+# Три товари з найкращим співвідношенням рейтингу та ціни
+def db_top_three(db_name: str) -> dict[str, Any]:
+    with connect('.\\' + db_name) as con:
+        con.row_factory = Row
+        result = con.execute('''
+                    SELECT title, url, rating, reviews,
+                           CAST(current_price AS REAL)/100 AS current_price,
+                           CAST(base_price AS REAL)/100 AS base_price,
+                           (CASE delivery
+                                WHEN 1 THEN 'Yes'
+                                ELSE 'No'
+                           END) AS delivery
+                    FROM products
+                    WHERE current_price IS NOT NULL AND rating IS NOT NULL
+                    ORDER BY (rating / current_price) DESC
+                    LIMIT 3
+                    ''').fetchone()
+    con.close()
+    return result
