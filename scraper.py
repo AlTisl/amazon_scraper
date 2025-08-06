3696fa3262d8e57f48ccd70cf4b14928b6b7e8c3
Olexandr Tislichenko <47416402+AlTisl@users.noreply.github.com>
2025-08-06 13:40:19 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -31,6 +31,7 @@ class amazon_scraper:
         options.add_argument('--disable-blink-features=AutomationControlled')
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument('--headless')
 
         user_agents = [
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -171,3 +172,4 @@ class amazon_scraper:
         finally:
 
             return self.products
+
